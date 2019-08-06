5d785a6a98b686329256094bb13cce001900a84a
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-05 20:54:12 -0400

    Changed orientation to horizontal

--- src/components/Work.js
+++ src/components/Work.js
@@ -34,6 +34,7 @@ const Work = React.memo(() => {
           pageBackground="#f2f3ee"
           showSwipeHint="true"
           uncutPages="true"
+          orientation="horizontal"
         >
           <section style={flipBookStyles}>
             <h1 className="text-header animated lightSpeedIn">
