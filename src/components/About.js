14e5d9943320951e265c2d8c282f94f8300f5984
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-07 20:08:24 -0400

    Finished adjusting media queries

--- src/components/About.js
+++ src/components/About.js
@@ -46,8 +46,8 @@ const About = React.memo(() => {
     return (
       <div className="aboutContainer">
         <Sidebar />
-        <div className="homeInfo">
-          <h1 className="text-header animated lightSpeedIn aboutHeader">
+        <div className="aboutInnerContainer">
+          <h1 className="text-header animated lightSpeedIn">
             <SplitText charPoses={charPoses}>
               Hello, my name is Isaiah Francois.
             </SplitText>
@@ -63,11 +63,25 @@ const About = React.memo(() => {
               <br />
               <br />
               <SplitText charPoses={charPoses}>
-                Since beginning my journey as a fullstack developer, I've
-                created many sites, designed games/apps , and have built and
-                modified backend servers. I'm confident in my skills, always
-                learning, and can teach what i know to others.
+                Everything started with a passion for learning advanced
+                technologies.
               </SplitText>
+              <br />
+              <SplitText charPoses={charPoses}>
+                2016 - Devloped an interest for coding an began studying HTML &
+                CSS.
+              </SplitText>
+              <br />
+              <SplitText charPoses={charPoses}>
+                2018 - Started developing computer game in unity while learning
+                C#.
+              </SplitText>
+              <br />
+              <SplitText charPoses={charPoses}>
+                2019 - Finished unity game and ported to Android. Also, joined
+                Lambda School to further my technoligical studies
+              </SplitText>
+              <br />
             </p>
           </div>
         </div>
